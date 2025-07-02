577c83a3237a4dcd20c5612d00c0504190936d3a
Ayushman-mnnit <araj53062@gmail.com>
2025-07-03 00:26:15 +0530

    style: improved spinner UI

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,7 @@
-import React from 'react'
-
 const Spinner = () => {
   return (
-    <div>
-      <div className='spinner'></div>
+    <div className="flex justify-center items-center h-[200px]">
+      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-white"></div>
     </div>
-  )
-}
-
-export default Spinner
+  );
+};
