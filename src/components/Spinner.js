7f6120e3b6d5004c4d4f388f4b08af3069163c66
Ayushman-mnnit <araj53062@gmail.com>
2025-07-03 17:50:59 +0530

    feat: improved Spinner with glow, gradient and bigger size

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,11 @@
+import React from "react";
+
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-[200px]">
-      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-white"></div>
+      <div className="w-16 h-16 border-4 border-t-transparent border-blue-400 border-solid rounded-full animate-spin shadow-lg shadow-blue-500"></div>
     </div>
   );
 };
+
+export default Spinner;
