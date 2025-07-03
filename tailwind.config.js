cd9cdce5ac08c8868e58d3d3767fccf29e9f4a7c
Ayushman-mnnit <araj53062@gmail.com>
2025-07-03 18:02:52 +0530

    feat: added animated gradient background to page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // 🌙 enable dark mode with class
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       animation: {
-        fadeIn: "fadeIn 0.5s ease-in-out forwards",
+        'gradient-x': 'gradient-x 5s ease infinite',
       },
       keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
+        'gradient-x': {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
         },
       },
     },
